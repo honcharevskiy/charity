51599e92db8427eb1aca01c45bef7cf3aaceb4ea
Nickolay Honcharevskyi <goncharevskiyy@gmail.com>
2024-06-23 09:41:42 +0300

    Allow CORS

--- charity/settings.py
+++ charity/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'mdeditor',
     'storages',
     'drf_spectacular',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -55,10 +56,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 50,
 }
+CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
